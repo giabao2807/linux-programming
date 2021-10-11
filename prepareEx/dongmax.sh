52c92e841c2c148ce9360c100378e17deb45764f
GiaBaone <102190252@sv1.dut.udn.vn>
2021-10-11 11:46:50 +0700

    update review 11.10.21

---
+++ prepareEx/dongmax.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+echo "Chuong trinh tim dong dai nhat trong tap tin $1"
+n=0
+max=0
+dong=""
+while read line
+do
+    n=`expr length "$line"`
+    if [ $n –gt $max ]
+    then
+        dong="$line"
+        max=$n
+    fi
+done
+echo "Dong trong tap tin $1 co do dai max = $max la : $dong"
+
+exit 0
