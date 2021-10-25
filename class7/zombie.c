ef900db192213764bdb971bda5ec94fb94665fa9
GiaBaone <102190252@sv1.dut.udn.vn>
2021-10-25 19:35:03 +0700

    shared memory - map memory 25.10.21

---
+++ class7/zombie.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    int child_pid;
+    child_pid = fork();
+    if (child_pid > 0)
+    {
+        printf("Day la tien trinh cha. sleep 1p \n");
+        sleep(60);
+    }
+    else
+    {
+        exit(0); /*Day la tien trinh con.Ket thuc */
+    }
+    return 0;
+}
